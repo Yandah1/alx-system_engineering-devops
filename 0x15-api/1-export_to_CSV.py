1334e6a6b89edeea4bc7cc353f07c3c24dbc0abd
yandah1 <nonku003@gmail.com>
2024-01-29 23:34:41 +0200

    FALSE

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,14 +10,13 @@ import sys
 API_URL = 'https://jsonplaceholder.typicode.com'
 '''The API's URL.'''
 
-
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
-            user_id = int(sys.argv[1])
-            user_res = requests.get(f'{API_URL}/users/{user_id}').json()
-            todos_res = requests.get(f'{API_URL}/todos').json()
-            user_name = user_res.get('name')
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+            todos_res = requests.get('{}/todos'.format(API_URL)).json()
+            user_name = user_res.get('username')
             todos = list(filter(lambda x: x.get('userId') == id, todos_res))
             with open('{}.csv'.format(id), 'w') as file:
                 for todo in todos:
